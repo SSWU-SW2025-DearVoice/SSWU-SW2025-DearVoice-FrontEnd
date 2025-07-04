50825156f56cbf7a8f07ec2113f369e9b21eea60
riveryunny <gayunphone@gmail.com>
2025-07-04 14:14:57 +0900

    feat: mypage, sentlist UI 구현

--- dearvoice/src/pages/mypage/MypageHome.jsx
+++ dearvoice/src/pages/mypage/MypageHome.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import '../../styles/MypageHome.css'
+import sentimg from "../../assets/images/letter-after.png";
+import receivedimg from "../../assets/images/letter-before.png";
+import { useNavigate } from "react-router-dom"; // 추가
 
 const MypageHome = () => {
+  const navigate = useNavigate(); // 추가
+
   return (
-    <div>MypageHome</div>
+    <div className="mypage-title">
+      <h2 className="mypage-top">내 보관소</h2>
+      <div className="mypage-container">
+        <button
+          className="mypage-sent"
+          onClick={() => navigate("/mypage/sent")} // 추가
+        >
+          <img src={sentimg} className='sentimg' alt="보낸 편지함"/>
+          <div className="sent-text">보낸 편지함</div>
+        </button>
+        <button className="mypage-received"
+        onClick={() => navigate("/mypage/received")} >
+          <div className="circle"></div>
+          <img src={receivedimg} className='receivedimg' alt="받은 편지함"/>
+          <div className="received-text">받은 편지함</div>
+        </button>
+      </div>
+    </div>
   )
 }
 
